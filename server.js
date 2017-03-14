0bf54ac999d6e8c8f1573b3b9c9799210fdbb6bc
steven <steven@ydongj.com>
2017-03-14 18:19:10 +0800

    production

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 /* eslint-disable */
-var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var config = require('./webpack.config');
+// var webpack = require('webpack');
+// var webpackDevMiddleware = require('webpack-dev-middleware');
+// var webpackHotMiddleware = require('webpack-hot-middleware');
+// var config = require('./webpack.config');
 var httpProxy = require('http-proxy');
 var app = new (require('express'))();
 var port = 3300;
 global.__COOKIE__ = null;
 
-
-var compiler = webpack(config);
+// var compiler = webpack(config);
 var proxy = httpProxy.createProxyServer({
   target: 'http://localhost:3060'
 });
 
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-app.use(webpackHotMiddleware(compiler));
+// app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+// app.use(webpackHotMiddleware(compiler));
+// app.use(compiler);
 
 app.get("/", function(req, res) {
   global.__COOKIE__ = req.get("cookie");
@@ -24,6 +24,14 @@ app.get("/", function(req, res) {
 
 app.use("/api",function(req,res){
   proxy.web(req,res,{target: 'http://localhost:3060'});
+});
+
+app.use("/static/bundle.js",function(req,res){
+  res.sendFile(__dirname + '/dist/bundle.js');
+});
+
+app.use("/static/vendor.js",function(req,res){
+  res.sendFile(__dirname + '/dist/vendor.js');
 });
 
 app.listen(port, function(error) {
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,39 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './index.js'
-  ],
+  // devtool: 'source-map',
+  devtool: false,
+  entry: {
+    bundle: [
+      './index.js'
+    ],
+    vendor: ['react']
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].js',
     publicPath: '/static/'
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    // new webpack.optimize.UglifyJsPlugin()
+    // new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false,  // remove all comments
+      },
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",//和上面配置的入口对应
+      filename: "vendor.js"//导出的文件的名称)
+    })
   ],
   module: {
     rules: [
